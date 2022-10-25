9b29fabd1669276cc555417768d5230e80a3e057
Marianne Bousquet <marianne.bousquet1@gmail.com>
2022-10-25 10:29:16 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Master!");
+        }
+    }
+}
